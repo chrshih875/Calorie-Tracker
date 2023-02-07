0550eba0d1395590351ac4e9e06743011162e39d
Christopher <chrshih875@gmail.com>
2023-02-06 16:18:21 -0800

    Docker/postman works!

--- Users/Migrations/20230206235614_SecondMigration.Designer.cs
+++ Users/Migrations/20230206235614_SecondMigration.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Users.Models;
 
@@ -10,9 +11,11 @@ using Users.Models;
 namespace Users.Migrations
 {
     [DbContext(typeof(UsersContext))]
-    partial class UsersContextModelSnapshot : ModelSnapshot
+    [Migration("20230206235614_SecondMigration")]
+    partial class SecondMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
