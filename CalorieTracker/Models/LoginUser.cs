04cb5a6e9f5c4eab019a8db97da2355356aa24b3
Christopher <chrshih875@gmail.com>
2023-02-06 16:18:21 -0800

    Docker/postman works!

--- CalorieTracker/Models/LoginUser.cs
+++ CalorieTracker/Models/LoginUser.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD:CalorieTracker/Models/LoginUser.cs
 // Disabled because we know the framework will assign non-null values when it
 // constructs this class for us.
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
@@ -21,3 +22,26 @@ public class LoginUser
     [Display(Name = "Password")]
     public string LoginPassword { get; set; }
 }
+=======
+// Disabled because we know the framework will assign non-null values when it
+// constructs this class for us.
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+[NotMapped]
+public class LoginUser
+{
+    [Required(ErrorMessage = "is required")]
+    [EmailAddress]
+    [Display(Name = "Email")]
+    public string LoginEmail { get; set; }
+
+    [Required(ErrorMessage = "is required")]
+    [MinLength(8, ErrorMessage = "must be at least 8 characters")]
+    [DataType(DataType.Password)]
+    [Display(Name = "Password")]
+    public string LoginPassword { get; set; }
+}
+>>>>>>> 0550eba0 (Docker/postman works!):Users/Models/LoginUser.cs
