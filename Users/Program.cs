18d22354f4935556daa4d56cccc31d945e9dd815
Christopher <chrshih875@gmail.com>
2023-02-09 13:22:18 -0800

    enable cors access

--- Users/Program.cs
+++ Users/Program.cs
@@ -1,9 +1,20 @@
 using Users.Models;
 using Microsoft.EntityFrameworkCore;
 
+var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddSession();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                    policy  =>
+                    {
+                        policy.WithOrigins("http://localhost:3000");
+                    });
+});
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<UsersContext>(options =>
@@ -11,12 +22,15 @@ builder.Services.AddDbContext<UsersContext>(options =>
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
 });
 
+
 var app = builder.Build();
 
 app.UseStaticFiles();
 app.UseRouting();
+app.UseCors(MyAllowSpecificOrigins);
 app.UseAuthorization();
 app.UseSession();
+
 
 if (!app.Environment.IsDevelopment())
 {
